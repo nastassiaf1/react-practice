651ec887c52dd63c5413b2e34ae73c905f8211a2
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-01-23 17:17:44 +0400

    start react router tutorial

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
+import * as React from "react";
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from "react-router-dom";
+
+import router from "./router";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
